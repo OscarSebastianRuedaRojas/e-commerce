387d455b1618f2dba80a8663adeca69ce5cde784
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-02-04 22:16:08 -0500

    feat: :rocket: Añadida Cuenta de Pago
    Se realizo todo el despliegue del menu de pagos, incluyendo su html, css y javascript
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-// let previsualizacion = document.querySelector('.previsualizacion');
-// document.querySelector('.boton').addEventListener('click',
-// function() {
-//     previsualizacion.classList.toggle('active')
-// });
 let botones = document.querySelectorAll('.boton')
 let previsualizacion = document.querySelector('.previsualizacion');
+let cuenta = document.querySelector('.cuenta');
+let cerrarCuenta = document.querySelector('.cuenta');
+let cerrar = document.querySelector('.previsualizacion');
 botones.forEach(function (button) {
     button.addEventListener('click', function () {
         previsualizacion.classList.add('active');
     });
 });
+document.querySelector('.x').addEventListener('click',
+function() {
+    cerrar.classList.toggle('active')
+ }); 
+document.querySelector('.carrito').addEventListener('click',
+function() {
+    cuenta.classList.toggle('active')
+ }); 
+ document.querySelector('.xCuenta').addEventListener('click',
+ function() {
+    cerrarCuenta.classList.toggle('active')
+  }); 
